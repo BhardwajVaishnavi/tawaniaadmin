8b7d873b6d11eb6893beedea38e0499c584f7513
Vaishnavi Bhardwaj <vaishnavibhardwaj401@gmail.com>
2025-05-05 04:03:14 +0530

    df

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// Create a custom datasources configuration to use the direct URL
-const datasources = {
-  db: {
-    url: process.env.DIRECT_URL,
-  },
-};
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    datasources: datasources,
-  });
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
